a54267b4cd439371e74d834d9d25aa1366164e9f
eeverywheree <39887288+eeverywheree@users.noreply.github.com>
2018-06-05 00:56:59 +0200

    Update commande.js

--- .gitignore/commandes/commande.js
+++ .gitignore/commandes/commande.js
@@ -1,17 +1,16 @@
-module.exports = class commande {
+module.exports = class Commande {
 
-static parse (message) {
-    if (this.match(message)) {
-        this.action(message)
-        return true
-    }
-    return false
-}
-
-    static match(message) {
+    static parse (message) {
+        if (this.match(message)) {
+            this.action(message)
+            return true
+        }  
         return false
     }
 
+    static match (message) {
+      return false  
+    }
 
     static action (message) {
     }
