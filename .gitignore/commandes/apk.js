8f80c1aea9d4ed5e41524e30a6b2e138cb668ac7
eeverywheree <39887288+eeverywheree@users.noreply.github.com>
2018-06-05 00:32:53 +0200

    Create apk.js

---
+++ .gitignore/commandes/apk.js
@@ -0,0 +1,13 @@
+module.exports = class Apk {
+
+
+    static match(message) {
+        return message.content.startsWith(',apk')
+    }
+
+
+    static action (message) {
+        message.channel.send('US https://www.apkmonk.com/app/com.spacegame.cashshow/\nUK https://www.apkmonk.com/app/com.spacegame.cashshow.gb/\nDE https://www.apkmonk.com/app/com.spacegame.cashshow.de/\nAU https://www.apkmonk.com/app/com.spacegame.cashshow.au/')
+    }
+
+}
