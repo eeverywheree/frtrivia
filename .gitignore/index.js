a8dd1a4dd87c3fdce7b0182941f646b6ee08337d
eeverywheree <39887288+eeverywheree@users.noreply.github.com>
2018-06-05 00:31:43 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,5 +1,6 @@
 const Discord = require('discord.js')
 const bot = new Discord.Client()
+const Apk = require('./commandes/apk')
 
 bot.on('ready', function () {
   console.log("Connecté !")
@@ -12,7 +13,7 @@ bot.on('ready', function () {
 })
 
 bot.on('message', function (message) {
-  if (message.content === ',apk') {
-    message.channel.send('US https://www.apkmonk.com/app/com.spacegame.cashshow/\nUK https://www.apkmonk.com/app/com.spacegame.cashshow.gb/\nDE https://www.apkmonk.com/app/com.spacegame.cashshow.de/\nAU https://www.apkmonk.com/app/com.spacegame.cashshow.au/')
+  if (Apk.match(message)) {
+    return Google.action (message)
   }
 })
