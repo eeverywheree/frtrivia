33e7f219eca28e6d44c92aee29fef6a14e6fa3d3
eeverywheree <39887288+eeverywheree@users.noreply.github.com>
2018-06-04 21:23:33 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -6,3 +6,13 @@ bot.on('ready', function () {
 })
 
 bot.login(process.env.TOKEN)
+
+bot.on('ready', function () {
+  bot.user.setActivity('Cash Show', { type: 'WATCHING' })
+})
+
+bot.on('message', function (message) {
+  if (message.content === ',apk') {
+    message.channel.send('US https://www.apkmonk.com/app/com.spacegame.cashshow/\nUK https://www.apkmonk.com/app/com.spacegame.cashshow.gb/\nDE https://www.apkmonk.com/app/com.spacegame.cashshow.de/\nAU https://www.apkmonk.com/app/com.spacegame.cashshow.au/')
+  }
+})
